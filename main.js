109bd8bfc002da986c6736b2c9bf3c2b0c771637
OxCart00 <107891625+OxCart00@users.noreply.github.com>
2022-10-24 17:12:24 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 // Variables
+//e3
+let matriz1 = [[1,0,0],[0,1,1],[0,1,0]]
+let contador1 = 0;
 //Funciones
 function e1() {
 }
 function e2() {
 }
-function e3() {
+function e3(a) {
+  for (let i = 0; i < a.length; i++) {
+    for (let j = 0; j < a.length; j++) {
+      if (a[i][j] == 0) {
+        contador1 ++;
+      }
+    }    
+  }
+  console.log("La cantidad de ceros en la matriz es: ",contador1);
 }
 function e4() {
 }
@@ -14,7 +25,7 @@ function e5() {
 function main() {
   e1();
   e2();
-  e3();
+  e3(matriz1);
   e4();
   e5();
 }

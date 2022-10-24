32de942cb4a8d39235475e9cd4b8cd7fa73a1de8
OxCart00 <107891625+OxCart00@users.noreply.github.com>
2022-10-24 15:43:20 -0600

    Update main.js
    ejercicio 1

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 // Variables
+// e1
+let lista1 = [-1,-2,-3,-14,-5,-6,-7,1,-2,24];
+let mayor = 0;
+// e2
+
+// e3
+
+// e4
+
+// e5
+
 //Funciones
-function e1() {
+function e1(a) {
+  for (let i = 0; i < lista1.length; i++) {
+    if (i == 0) {
+      mayor = lista1[i];
+    }
+    if (lista1 [i] > mayor) {
+      mayor = lista1 [i];
+    }
+  }
+  console.log(mayor);
 }
 function e2() {
 }

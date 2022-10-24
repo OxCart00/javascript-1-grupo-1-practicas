807b606d6bf13e57009f21b02462bf3cddd096e4
OxCart00 <107891625+OxCart00@users.noreply.github.com>
2022-10-24 15:29:53 -0600

    Update main.js
    Definicion del esqueleto de trabajo

---
+++ main.js
@@ -0,0 +1,21 @@
+// Variables
+//Funciones
+function e1() {
+}
+function e2() {
+}
+function e3() {
+}
+function e4() {
+}
+function e5() {
+}
+//Ejecucion de Funciones
+function main() {
+  e1();
+  e2();
+  e3();
+  e4();
+  e5();
+}
+main();

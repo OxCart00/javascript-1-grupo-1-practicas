b9f7a4a5114204ddb28bc87fd4e15e8608010a5e
OxCart00 <107891625+OxCart00@users.noreply.github.com>
2022-11-19 01:14:52 -0600

    Update main.js

--- main.js
+++ main.js
@@ -18,32 +18,32 @@ let space_sentence = "Draco dormiens nunquam titillandus";
 // Definicion de funciones
 // E1
 function E1(a) {
-  if (a == 0 ) {
+  if (a == 0) {
     return 1;
   }
-  return a*E1(a-1);
+  return a * E1(a - 1);
 }
 
 // E2
 function E2(a) {
   if (((a % 4 == 0) && (a % 100 != 0)) || (a % 400 == 0)) {
-    console.log("El año",a,"SI es bisiesto\n");
-  }else{
-    console.log("El año",a,"NO es bisiesto\n");
+    console.log("El año", a, "SI es bisiesto\n");
+  } else {
+    console.log("El año", a, "NO es bisiesto\n");
   }
 }
 
 // E3
-function E3(a,b) {
+function E3(a, b) {
   let counter = 0;
   a = a.toLowerCase();
   b = b.toLowerCase();
   for (let i = 0; i < a.length; i++) {
     if (a[i] == b) {
-      counter ++;
+      counter++;
     }
   }
-  console.log("El caracter",b,"se repite",counter,"veces en:",a,"\n");
+  console.log("El caracter", b, "se repite", counter, "veces en:", a, "\n");
 }
 
 // E4
@@ -53,16 +53,16 @@ function E4(a) {
   let tries = Math.floor(Math.random() * (max - min + 1) + min);
   while (tries != a && tries != 0) {
     if (tries > a) {
-    console.log(tries,"es mayor que el numero secreto");
-    }else{
-    console.log(tries,"es menor que el numero secreto");
+      console.log(tries, "es mayor que el numero secreto");
+    } else {
+      console.log(tries, "es menor que el numero secreto");
     }
     tries = Math.floor(Math.random() * (max - min + 1) + min);
   }
   if (tries == 0) {
     console.log("Programa detenido\n");
-  }else{
-    console.log(tries,"Si es el numero secreto\n");
+  } else {
+    console.log(tries, "Si es el numero secreto\n");
   }
 }
 
@@ -74,14 +74,14 @@ function E5(a) {
       without_spaces += a[i];
     }
   }
-  console.log("La palabra sin espacios es:",without_spaces,"\n");
+  console.log("La palabra sin espacios es:", without_spaces, "\n");
 }
 
 // LLamado de funciones
 function main() {
   // E1
   console.log("Respuesta Ejercicio #1\n");
-  console.log("El factorial de",factorial_number,"es:",E1(factorial_number),"\n");
+  console.log("El factorial de", factorial_number, "es:", E1(factorial_number), "\n");
 
   // E2
   console.log("Respuesta Ejercicio #2\n");
@@ -89,7 +89,7 @@ function main() {
 
   // E3
   console.log("Respuesta Ejercicio #3\n");
-  E3(sentence,character);
+  E3(sentence, character);
 
   // E4
   console.log("Respuesta Ejercicio #4\n");

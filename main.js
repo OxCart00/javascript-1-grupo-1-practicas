9531b1dfe17bb07aa36ffdfe29ae12ef012e1827
OxCart00 <107891625+OxCart00@users.noreply.github.com>
2022-11-19 00:44:06 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,102 +1,102 @@
 // Variables
-// e1
-let lista1 = [-1,-2,-3,-14,-5,-6,-7,1,-2,24];
-let mayor = 0;
-//e2
-let frase1 = "Lol";
-let analisis1 = "";
-let validacion1 = 1;
-//e3
-let matriz1 = [[1,0,0],[0,1,1],[0,1,0]]
-let contador1 = 0;
-//e4
-let lista2 = [2,3,4,5,6,12,-1,-24,0,2,2,2,3,5,4,-1,2];
-let repetidos = [];
-//e5
-let largo = 8;
-let patron = "";
-//Funciones
-function e1(a,b) {
+//E1
+let factorial_number = 6;
+
+// E2
+let year = 2017;
+
+// E3
+let sentence = "Viva el futbol Siuuuuuuuuu";
+let character = "U";
+
+// E4
+let secret_number = 12;
+
+// E5
+let space_sentence = "Draco dormiens nunquam titillandus";
+
+// Definicion de funciones
+// E1
+function E1(a) {
+  if (a == 0 ) {
+    return 1;
+  }
+  return a*E1(a-1);
+}
+
+// E2
+function E2(a) {
+  if (((a%4 == 0) & (a%100 != 0)) || (a%400==0)) {
+    console.log("El año",a,"SI es bisiesto\n");
+  }else{
+    console.log("El año",a,"NO es bisiesto\n");
+  }
+}
+
+// E3
+function E3(a,b) {
+  let contador = 0;
+  a = a.toLowerCase();
+  b = b.toLowerCase();
   for (let i = 0; i < a.length; i++) {
-    if (i == 0) {
-      b = a[i];
-    }
-    if (a [i] > b) {
-      b = a [i];
+    if (a[i] == b) {
+      contador ++;
     }
   }
-  console.log("El numero mayor es:",b,"\n");
+  console.log("El caracter",b,"se repite",contador,"veces en:",a,"\n");
 }
-function e2(a,b,c) {
+
+// E4
+function E4(a) {
+  let min = 0;
+  let max = 100;
+  let i = Math.floor(Math.random() * (max - min + 1) + min);
+  while (a != i && i != 0) {
+    if (i > a) {
+    console.log(i,"es mayor que el numero secreto");
+    }else{
+    console.log(i,"es menor que el numero secreto");
+    }
+    i = Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  if (i == 0) {
+    console.log("Programa detenido\n");
+  }else{
+    console.log(i,"Si es el numero secreto\n");
+  }
+}
+
+// E5
+function E5(a) {
+  let sin_espacios = "";
   for (let i = 0; i < a.length; i++) {
     if (a[i] != " ") {
-      b += a[i];
-    }    
-  }
-  b = b.toLowerCase();
-  let j = b.length-1;
-  for (let i = 0; i < b.length; i++) {
-    if (b[i] != b[j]) {
-      c = 0;
-    }
-    j--;
-  }
-  if (c == 1) {
-    console.log("La frase:", a,"SI es palindromo\n");    
-  }else{
-    console.log("La frase:", a,"NO es palindromo\n");
-  }
-}
-function e3(a) {
-  for (let i = 0; i < a.length; i++) {
-    for (let j = 0; j < a.length; j++) {
-      if (a[i][j] == 0) {
-        contador1 ++;
-      }
-    }    
-  }
-  console.log("La cantidad de ceros en la matriz es: ",contador1,"\n");
-}
-function e4(a,b) {
-  for (let i = 0; i < a.length; i++) {
-    let repeticiones = 0;
-    for (let j = 0; j < a.length; j++) {
-      if (a[i] == a[j] && i != j && repeticiones == 0) {
-        b.push(a[i]);
-        a.splice(j,1);
-        j--;
-        repeticiones = 1;
-      }
-      if (a[i] == a[j] && i != j && repeticiones == 1) {
-        a.splice(j,1);
-        j--;
-      }
+      sin_espacios += a[i];
     }
   }
-  if (b.length > 0) {
-    console.log("Los numeros repetidos son: ",b,"\n");
-  }else{
-    console.log("No se encontraron numeros repetidos en la lista de numeros recibidos\n");
-  }
+  console.log("La palabra sin espacios es:",sin_espacios,"\n");
 }
-function e5(a,b) {
-  console.log("Triangulo tamaño:",a,"\n");
-  for (let i = 1; i <= a; i++) {
-    b += "* ";
-    console.log(b);
-  }
-}
-//Ejecucion de Funciones
+
+// LLamado de funciones
 function main() {
+  // E1
   console.log("Respuesta Ejercicio #1\n");
-  e1(lista1,mayor);
+  console.log("El factorial de",factorial_number,"es:",E1(factorial_number),"\n");
+
+  // E2
   console.log("Respuesta Ejercicio #2\n");
-  e2(frase1,analisis1,validacion1);
+  E2(year);
+
+  // E3
   console.log("Respuesta Ejercicio #3\n");
-  e3(matriz1);
+  E3(sentence,character);
+
+  // E4
   console.log("Respuesta Ejercicio #4\n");
-  e4(lista2,repetidos);
+  E4(secret_number);
+
+  // E5
   console.log("Respuesta Ejercicio #5\n");
-  e5(largo,patron);
+  E5(space_sentence);
 }
 main();
